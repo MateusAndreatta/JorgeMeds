320f39fc7b6dd3c8ccf7c8797cc4e14ba0e3c415
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2024-06-04 20:28:47 -0300

    Initial Commit

---
+++ JorgeMeds/JorgeMedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JorgeMedsApp.swift
+//  JorgeMeds
+//
+//  Created by Mateus Andreatta on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct JorgeMedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
